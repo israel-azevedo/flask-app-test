85359744135a2b279060332b910d2818c6e906f5
israel-azevedo <80106486+israel-azevedo@users.noreply.github.com>
2024-11-02 15:59:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello, OpenShift!'
 
+@app.route('/health')
+def health():
+    return 'Healthy', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
